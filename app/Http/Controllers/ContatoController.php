61c69100d78e1f6c6ee8feee802169444db410f6
João Kaíque <joaok231@gmail.com>
2021-02-21 15:37:30 -0300

    Definicao dos esqueletos em ContatoController

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,9 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    //
+    private $repository;
+
+    public function __construct(Contato $contato)
+    {
+        $this->repository = $contato;
+    }
+
+    public function index()
+    {
+        
+    }
+
+    public function show($id)
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function edit($id)
+    {
+
+    }
+
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    public function delete($id)
+    {
+
+    }
 }
